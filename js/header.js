a2ac80c2794557cec892253b935a73df1ee3dbac
Isaac <sdaniel9391@gmail.com>
2024-08-23 14:47:29 +0200

    cambio al desplegable del header

---
+++ js/header.js
@@ -0,0 +1,21 @@
+let desplegable = document.getElementById("desplegable");
+let despliege  = document.getElementById("despliege");
+
+//Mouse over icono usuario
+desplegable.addEventListener("mouseover", function(){
+    despliege.style.display = "flex";
+})
+
+desplegable.addEventListener("mouseout", function(){
+    despliege.style.display = "none";
+})
+
+
+//Mouse over en el div
+despliege.addEventListener("mouseover", function(){
+    despliege.style.display = "flex";
+})
+
+despliege.addEventListener("mouseout", function(){
+    despliege.style.display = "none";
+})
